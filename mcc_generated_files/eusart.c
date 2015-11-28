1e8ef43cda473eee215445b73c4cf86842f6752e
Ken Okabe <ken@design-fun.com>
2015-11-28 14:28:03 +0900

    initial uart test a-z

---
+++ mcc_generated_files/eusart.c
@@ -0,0 +1,228 @@
+/**
+  EUSART Generated Driver File
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    eusart.c
+
+  @Summary
+    This is the generated driver implementation file for the EUSART driver using MPLAB® Code Configurator
+
+  @Description
+    This header file provides implementations for driver APIs for EUSART.
+    Generation Information :
+        Product Revision  :  MPLAB® Code Configurator - v2.25.2
+        Device            :  PIC16F1509
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 v1.34
+        MPLAB             :  MPLAB X v2.35 or v3.00
+ */
+
+/*
+Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ */
+
+/**
+  Section: Included Files
+ */
+#include "eusart.h"
+
+/**
+  Section: Macro Declarations
+ */
+#define EUSART_TX_BUFFER_SIZE 8
+#define EUSART_RX_BUFFER_SIZE 8
+
+/**
+  Section: Global Variables
+ */
+
+static uint8_t eusartTxHead = 0;
+static uint8_t eusartTxTail = 0;
+static uint8_t eusartTxBuffer[EUSART_TX_BUFFER_SIZE+1];
+volatile uint8_t eusartTxBufferRemaining;
+
+static uint8_t eusartRxHead = 0;
+static uint8_t eusartRxTail = 0;
+static uint8_t eusartRxBuffer[EUSART_RX_BUFFER_SIZE];
+volatile uint8_t eusartRxCount;
+
+/**
+  Section: EUSART APIs
+ */
+
+void EUSART_Initilize(void) {
+    // disable interrupts before changing states
+    PIE1bits.RCIE = 0;
+    PIE1bits.TXIE = 0;
+
+    // Set the EUSART module to the options selected in the user interface.
+
+    // ABDOVF no_overflow; SCKP async_noninverted_sync_fallingedge; RCIDL idle; BRG16 16bit_generator; WUE disabled; ABDEN disabled;
+    BAUDCON = 0x48;
+
+    // SPEN enabled; OERR no_error; RX9 8-bit; RX9D 0x0; CREN disabled; ADDEN disabled; SREN disabled; FERR no_error;
+    RCSTA = 0x80;
+
+    // TRMT TSR_empty; TX9 8-bit; TX9D 0x0; SENDB sync_break_complete; TXEN enabled; SYNC asynchronous; BRGH hi_speed; CSRC slave_mode;
+    TXSTA = 0x26;
+
+    // Baud Rate = 9600; SPBRGL 160;
+    SPBRGL = 0xA0;
+
+    // Baud Rate = 9600; SPBRGH 1;
+    SPBRGH = 0x01;
+
+
+    // initializing the driver state
+    eusartTxHead = 0;
+    eusartTxTail = 0;
+    eusartTxBufferRemaining = sizeof (eusartTxBuffer);
+
+    eusartRxHead = 0;
+    eusartRxTail = 0;
+    eusartRxCount = 0;
+
+    eusartTxBuffer[EUSART_TX_BUFFER_SIZE] = '\0'
+
+    // enable receive interrupt
+    PIE1bits.RCIE = 1;
+}
+
+void EUSART_NoTransmit(void) {
+    // disable interrupts before changing states
+    PIE1bits.RCIE = 0;
+    PIE1bits.TXIE = 0;
+
+    // Set the EUSART module to the options selected in the user interface.
+
+    // ABDOVF no_overflow; SCKP async_noninverted_sync_fallingedge; RCIDL idle; BRG16 16bit_generator; WUE disabled; ABDEN disabled;
+    BAUDCON = 0x48;
+
+    // SPEN enabled; OERR no_error; RX9 8-bit; RX9D 0x0; CREN disabled; ADDEN disabled; SREN disabled; FERR no_error;
+    RCSTA = 0x80;
+
+    // TRMT TSR_empty; TX9 8-bit; TX9D 0x0; SENDB sync_break_complete; TXEN disabled; SYNC asynchronous; BRGH hi_speed; CSRC slave_mode;
+    TXSTA = 0x06;
+
+    // Baud Rate = 9600; SPBRGL 160;
+    SPBRGL = 0xA0;
+
+    // Baud Rate = 9600; SPBRGH 1;
+    SPBRGH = 0x01;
+
+
+    // initializing the driver state
+    eusartTxHead = 0;
+    eusartTxTail = 0;
+    eusartTxBufferRemaining = sizeof (eusartTxBuffer);
+
+    eusartRxHead = 0;
+    eusartRxTail = 0;
+    eusartRxCount = 0;
+
+    // enable receive interrupt
+    PIE1bits.RCIE = 1;
+}
+
+uint8_t EUSART_Read(void) {
+    uint8_t readValue = 0;
+
+    while (0 == eusartRxCount) {
+    }
+
+    PIE1bits.RCIE = 0;
+
+    readValue = eusartRxBuffer[eusartRxTail++];
+    if (sizeof (eusartRxBuffer) <= eusartRxTail) {
+        eusartRxTail = 0;
+    }
+    eusartRxCount--;
+    PIE1bits.RCIE = 1;
+
+    return readValue;
+}
+
+void EUSART_Write(char *txData) {
+    while (eusartTxBufferWorkingFlag) {
+    }
+
+    PIE1bits.TXIE = 0;
+    while(1){
+      eusartTxBuffer[i] = txData[i];
+      if(txData[i++]=='\0'){
+        break;
+      }
+      if(i>=MAX_TXBUF){
+        break;
+      }
+    }
+
+    eusartTxBufferWorkingFlag = 1;
+    eusartTxTail = 0;
+
+    PIE1bits.TXIE = 1;
+}
+
+
+
+void EUSART_Transmit_ISR(void) {
+
+    // add your EUSART interrupt custom code
+    if(!eusartTxBufferWorkingFlag || (eusartTxBuffer[eusartTxTail]=='\0')){
+      PIE1bits.TXIE = 0;
+      eusartTxBufferWorkingFlag = 0;
+      // for debug
+      RC3=1;
+      return;
+    }
+
+    TXREG = eusartTxBuffer[eusartTxTail++];
+    if((eusartTxTail>=MAX_TXBUF) || (eusartTxBuffer[eusartTxTail]=='\0')){
+      PIE1bits.TXIE = 0;
+      eusartTxBufferWorkingFlag = 0;
+    }
+
+    return;
+}
+
+void EUSART_Receive_ISR(void) {
+    if (1 == RCSTAbits.OERR) {
+        // EUSART error - restart
+
+        RCSTAbits.CREN = 0;
+        RCSTAbits.CREN = 1;
+    }
+
+    // buffer overruns are ignored
+    eusartRxBuffer[eusartRxHead++] = RCREG;
+    if (sizeof (eusartRxBuffer) <= eusartRxHead) {
+        eusartRxHead = 0;
+    }
+    eusartRxCount++;
+}
+/**
+  End of File
+ */
---
+++ mcc_generated_files/pin_manager.h
@@ -0,0 +1,177 @@
+/**
+  @Generated Pin Manager Header File
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    pin_manager.h
+
+  @Summary:
+    This is the Pin Manager file generated using MPLAB® Code Configurator
+
+  @Description:
+    This header file provides implementations for pin APIs for all pins selected in the GUI.
+    Generation Information :
+        Product Revision  :  MPLAB® Code Configurator - v2.25.2
+        Device            :  PIC16F1509
+        Version           :  1.01
+    The generated drivers are tested against the following:
+        Compiler          :  XC8 v1.34
+        MPLAB             :  MPLAB X v2.35 or v3.00
+ */
+
+/*
+Copyright (c) 2013 - 2015 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ */
+
+#ifndef PIN_MANAGER_H
+#define PIN_MANAGER_H
+
+#define INPUT   1
+#define OUTPUT  0
+
+#define HIGH    1
+#define LOW     0
+
+#define ANALOG      1
+#define DIGITAL     0
+
+#define PULL_UP_ENABLED      1
+#define PULL_UP_DISABLED     0
+
+// get/set RX aliases
+#define RX_TRIS               TRISB5
+#define RX_LAT                LATB5
+#define RX_PORT               RB5
+#define RX_WPU                WPUB5
+#define RX_ANS                ANSB5
+#define RX_SetHigh()    do { LATB5 = 1; } while(0)
+#define RX_SetLow()   do { LATB5 = 0; } while(0)
+#define RX_Toggle()   do { LATB5 = ~LATB5; } while(0)
+#define RX_GetValue()         RB5
+#define RX_SetDigitalInput()    do { TRISB5 = 1; } while(0)
+#define RX_SetDigitalOutput()   do { TRISB5 = 0; } while(0)
+
+#define RX_SetPullup()    do { WPUB5 = 1; } while(0)
+#define RX_ResetPullup()   do { WPUB5 = 0; } while(0)
+#define RX_SetAnalogMode()   do { ANSB5 = 1; } while(0)
+#define RX_SetDigitalMode()   do { ANSB5 = 0; } while(0)
+// get/set TX aliases
+#define TX_TRIS               TRISB7
+#define TX_LAT                LATB7
+#define TX_PORT               RB7
+#define TX_WPU                WPUB7
+#define TX_SetHigh()    do { LATB7 = 1; } while(0)
+#define TX_SetLow()   do { LATB7 = 0; } while(0)
+#define TX_Toggle()   do { LATB7 = ~LATB7; } while(0)
+#define TX_GetValue()         RB7
+#define TX_SetDigitalInput()    do { TRISB7 = 1; } while(0)
+#define TX_SetDigitalOutput()   do { TRISB7 = 0; } while(0)
+
+#define TX_SetPullup()    do { WPUB7 = 1; } while(0)
+#define TX_ResetPullup()   do { WPUB7 = 0; } while(0)
+// get/set IO_RC0 aliases
+#define IO_RC0_TRIS               TRISC0
+#define IO_RC0_LAT                LATC0
+#define IO_RC0_PORT               RC0
+#define IO_RC0_ANS                ANSC0
+#define IO_RC0_SetHigh()    do { LATC0 = 1; } while(0)
+#define IO_RC0_SetLow()   do { LATC0 = 0; } while(0)
+#define IO_RC0_Toggle()   do { LATC0 = ~LATC0; } while(0)
+#define IO_RC0_GetValue()         RC0
+#define IO_RC0_SetDigitalInput()    do { TRISC0 = 1; } while(0)
+#define IO_RC0_SetDigitalOutput()   do { TRISC0 = 0; } while(0)
+
+#define IO_RC0_SetAnalogMode()   do { ANSC0 = 1; } while(0)
+#define IO_RC0_SetDigitalMode()   do { ANSC0 = 0; } while(0)
+// get/set IO_RC1 aliases
+#define IO_RC1_TRIS               TRISC1
+#define IO_RC1_LAT                LATC1
+#define IO_RC1_PORT               RC1
+#define IO_RC1_ANS                ANSC1
+#define IO_RC1_SetHigh()    do { LATC1 = 1; } while(0)
+#define IO_RC1_SetLow()   do { LATC1 = 0; } while(0)
+#define IO_RC1_Toggle()   do { LATC1 = ~LATC1; } while(0)
+#define IO_RC1_GetValue()         RC1
+#define IO_RC1_SetDigitalInput()    do { TRISC1 = 1; } while(0)
+#define IO_RC1_SetDigitalOutput()   do { TRISC1 = 0; } while(0)
+
+#define IO_RC1_SetAnalogMode()   do { ANSC1 = 1; } while(0)
+#define IO_RC1_SetDigitalMode()   do { ANSC1 = 0; } while(0)
+// get/set IO_RC2 aliases
+#define IO_RC2_TRIS               TRISC2
+#define IO_RC2_LAT                LATC2
+#define IO_RC2_PORT               RC2
+#define IO_RC2_ANS                ANSC2
+#define IO_RC2_SetHigh()    do { LATC2 = 1; } while(0)
+#define IO_RC2_SetLow()   do { LATC2 = 0; } while(0)
+#define IO_RC2_Toggle()   do { LATC2 = ~LATC2; } while(0)
+#define IO_RC2_GetValue()         RC2
+#define IO_RC2_SetDigitalInput()    do { TRISC2 = 1; } while(0)
+#define IO_RC2_SetDigitalOutput()   do { TRISC2 = 0; } while(0)
+
+#define IO_RC2_SetAnalogMode()   do { ANSC2 = 1; } while(0)
+#define IO_RC2_SetDigitalMode()   do { ANSC2 = 0; } while(0)
+// get/set IO_RC3 aliases
+#define IO_RC3_TRIS               TRISC3
+#define IO_RC3_LAT                LATC3
+#define IO_RC3_PORT               RC3
+#define IO_RC3_ANS                ANSC3
+#define IO_RC3_SetHigh()    do { LATC3 = 1; } while(0)
+#define IO_RC3_SetLow()   do { LATC3 = 0; } while(0)
+#define IO_RC3_Toggle()   do { LATC3 = ~LATC3; } while(0)
+#define IO_RC3_GetValue()         RC3
+#define IO_RC3_SetDigitalInput()    do { TRISC3 = 1; } while(0)
+#define IO_RC3_SetDigitalOutput()   do { TRISC3 = 0; } while(0)
+
+#define IO_RC3_SetAnalogMode()   do { ANSC3 = 1; } while(0)
+#define IO_RC3_SetDigitalMode()   do { ANSC3 = 0; } while(0)
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    GPIO and peripheral I/O initialization
+ * @Example
+    PIN_MANAGER_Initialize();
+ */
+void PIN_MANAGER_Initialize(void);
+
+/**
+ * @Param
+    none
+ * @Returns
+    none
+ * @Description
+    Interrupt on Change Handling routine
+ * @Example
+    PIN_MANAGER_IOC();
+ */
+void PIN_MANAGER_IOC(void);
+
+#endif // PIN_MANAGER_H
+/**
+ End of File
+ */
